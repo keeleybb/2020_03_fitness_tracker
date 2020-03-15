54b3dce7dfdcb1128b485d3ea5dc14e65371c2f5
keeleybb <keeleybb@users.noreply.github.com>
2020-03-15 16:09:23 -0600

    trying heroku deployment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,10 +12,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //Connecting/Creating my Db
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
